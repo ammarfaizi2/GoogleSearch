21e1f71ab043ef34fe331b0fc3a7a1cf837f6b24
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-27 17:15:58 +0700

    Prepare parse output

--- src/GoogleSearch.php
+++ src/GoogleSearch.php
@@ -32,6 +32,11 @@ final class GoogleSearch
 	private $cookiefile;
 
 	/**
+	 * @var string
+	 */
+	private $errorInfo;
+
+	/**
 	 * Constructor.
 	 *
 	 * @param string $query
@@ -64,7 +69,7 @@ final class GoogleSearch
 
 	private function search()
 	{
-		$ch = curl_init("https://www.google.com/search?client=ubuntu&channel=fs&q=".urlencode($this->query)."&ie=utf-8&oe=utf-8");
+		/*$ch = curl_init("https://www.google.com/search?client=ubuntu&channel=fs&q=".urlencode($this->query)."&ie=utf-8&oe=utf-8");
 		curl_setopt_array($ch, 
 			[
 				CURLOPT_RETURNTRANSFER 	=> true,
@@ -73,14 +78,40 @@ final class GoogleSearch
 				CURLOPT_CONNECTTIMEOUT 	=> 15,
 				CURLOPT_COOKIEFILE 		=> $this->cookiefile,
 				CURLOPT_COOKIEJAR 		=> $this->cookiefile,
-				CURLOPT_USERAGENT 		=> "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:56.0) Gecko/20100101 Firefox/56.0",
+				CURLOPT_USERAGENT 		=> "Opera/9.80 (J2ME/MIDP; Opera Mini/4.2/28.3590; U; en) Presto/2.8.119 Version/11.10. 4.2",
 				CURLOPT_TIMEOUT			=> 15
 			]
 		);
 		$out = curl_exec($ch);
 		$no  = curl_errno($ch) and $out = "Error ({$no}) : ".curl_error($ch);
-		file_put_contents("a.tmp", $out);
-		return $out;
+		file_put_contents("a.tmp", $out);*/
+		// return $out;
+		return file_get_contents("a.tmp");
+	}
+
+	private function parseOutput($out)
+	{
+		$a = explode("<div class=\"_Z1m\">", $out);
+		if (count($a) < 3) {
+			$this->errorInfo();
+			return false;
+		}
+		unset($a[0], $a[1]);
+		$results = [];
+		foreach ($a as $val) {
+			// get url
+			$b = explode("<a class=\"_Olt _bCp\" href=\"/url?q=", $val, 2);
+			if (isset($b[1])) {
+				$b = explode("\"", $b[1], 2);
+				$b = explode("&amp;", $b[0], 2);
+				$c = "";
+				$results[] = [
+					"url" => html_entity_decode($b[0], ENT_QUOTES, 'UTF-8')
+				];
+			}
+		}
+		var_dump($results);
+		die;
 	}
 
 	/**
@@ -90,6 +121,7 @@ final class GoogleSearch
 	 */
 	public function exec()
 	{
-		return $this->search();
+		$out = $this->search();
+		return $this->errorInfo ? $this->errorInfo : $this->parseOutput($out);
 	}
 }
